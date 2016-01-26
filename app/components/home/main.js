e5341af6adde2a5337789232777c4dfe87b2a4fa
cviegas <cviegas@azi.net.br>
2016-01-25 23:26:55 -0300

    Adicionado alguns componentes do boostrap ui

--- app/components/home/main.js
+++ app/components/home/main.js
@@ -20,6 +20,10 @@ define(['text!components/home/view.html'], function (view) {
                 $scope.singleModel = 1;
                 $scope.isCollapsed = false;
 
+                $scope.selected = undefined;
+
+                $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
                 $scope.items = [
                     'The first choice!',
                     'And another choice for you.',
@@ -41,6 +45,23 @@ define(['text!components/home/view.html'], function (view) {
                 };
 
                 $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+
+
+                // Any function returning a promise object can be used to load values asynchronously
+                $scope.getLocation = function(val) {
+                    return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+                        params: {
+                            address: val,
+                            sensor: false
+                        }
+                    }).then(function(response){
+                        return response.data.results.map(function(item){
+                            return item.formatted_address;
+                        });
+                    });
+                };
+
+
 
             }]]
         },
